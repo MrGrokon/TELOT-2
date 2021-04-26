c627f6160ca971ae9b9f47d86d6943cf6f45ca69
Lucas DUPUIS <lucas.dupuis.ican@gmail.com>
2021-04-26 18:00:58 +0200

    [26-04] Basic line renderer for laser

---
+++ Assets/Scripts/Enemies/EnemiesProps/LaserRendering_Behavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRendering_Behavior : MonoBehaviour
+{
+    private LineRenderer _LR;
+
+    void Start()
+    {
+        _LR = this.GetComponent<LineRenderer>();
+    }
+
+    private void Update(){
+        RaycastHit _hit;
+        Physics.Raycast(this.transform.position, this.transform.forward, out _hit, Mathf.Infinity);
+
+        _LR.SetPosition(0, new Vector3());
+        _LR.SetPosition(1, new Vector3(0f,0f, Vector3.Distance(this.transform.position, _hit.point)));
+    }
+}
