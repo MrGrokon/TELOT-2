38ec0769901ab265ecf5ce1b1643068462d9155b
Lucas DUPUIS <lucas.dupuis.ican@gmail.com>
2021-05-06 15:21:30 +0200

    [06-05] prefab ExplosionV1 à faire validé

---
+++ Assets/Scripts/Debug/TriggerAllChildren_PS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAllChildren_PS : MonoBehaviour
+{
+    public KeyCode ActivationKey = KeyCode.Space;
+
+    private List<ParticleSystem> PS_list = new List<ParticleSystem>();
+
+    void Start()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            PS_list.Add(this.transform.GetChild(i).GetComponent<ParticleSystem>());
+        }
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(ActivationKey)){
+            foreach (var _ps in PS_list)
+            {
+                _ps.Play();
+            }
+        }
+    }
+}
