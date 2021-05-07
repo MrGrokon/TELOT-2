dedf96ce5a15dbb434ad6cc664fb39e677bb04ab
Lucas DUPUIS <lucas.dupuis.ican@gmail.com>
2021-05-07 20:54:05 +0200

    [07-07] toom smoke on closing doors

---
+++ Assets/Scripts/Level Design/DoorAnimatorEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimatorEvents : MonoBehaviour
+{
+    ParticleSystem ToonSmoke;
+    List<ParticleSystem> Sparks;
+
+    void Start()
+    {
+        ToonSmoke = this.transform.GetChild(2).GetComponent<ParticleSystem>();
+    }
+
+    public void Call_Sparks(){
+        foreach(var ps in Sparks){
+            ps.Play();
+        }
+    }
+
+    public void Call_Smoke(){
+        ToonSmoke.Play();
+    }
+
+
+}
