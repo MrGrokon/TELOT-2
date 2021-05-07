aba84e62ca86ea6a6c6aa02edc4bf4624f19f402
Lucas DUPUIS <lucas.dupuis.ican@gmail.com>
2021-05-07 15:05:45 +0200

    [07-07] quick fixes -merged prob

--- Assets/Scripts/Navigation/RoomTrigger.cs
+++ Assets/Scripts/Navigation/RoomTrigger.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomTrigger : MonoBehaviour
 {
-    public List<EnemySpawner> Spawners;
+
+    public int roomNumber;
+    public List<EnemySpawner> Spawners_Wave1;
+    public List<EnemySpawner> Spawners_Wave2;
+
+    public float TimeUntilWaveTwo = 35f;
+    private float _elapsedWaveTime = 0f;
+    private bool WaveTwoLaunched = false;
+
     public List<Animator> DoorsAnimator;
+    public bool activeRoom;
 
     [HideInInspector]
     public List<MonsterBehavior> monsters;
@@ -15,12 +25,16 @@ public class RoomTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider _col) {
         if(_col.tag == "Player" && WaveIsActive == false)
         {
+            activeRoom = true;
             _musicManager = UI_Feedbacks.Instance.GetComponent<MusicManager>();
             _musicManager.StartMusic();
+
+            //ProgressionFollow PF = UI_Feedbacks.Instance.GetComponent<ProgressionFollow>();
+            //PF.UpdateRoomCount(roomNumber, 5);
             
             WaveIsActive = true;
             Debug.Log("Player enter a room");
-            foreach (var _spawn in Spawners)
+            foreach (var _spawn in Spawners_Wave1)
             {
                 monsters.Add(_spawn.SpawnAnEnemy());
                 //do some shit like closing door, etc ...
@@ -33,15 +47,33 @@ public class RoomTrigger : MonoBehaviour
     }
 
     private void Update() {
-        if(WaveIsActive == true && monsters.Count == 0){
-            Debug.Log("wave ended");
-            _musicManager.ChangeSituation(2);
-            foreach (var door in DoorsAnimator)
+        if (activeRoom)
+        {
+            _elapsedWaveTime += Time.deltaTime;
+            //ProgressionFollow PF = UI_Feedbacks.Instance.GetComponent<ProgressionFollow>();
+            //PF.UpdateEnnemiesRemainingCount(monsters.Count);  
+        }
+        
+        if(WaveIsActive == true && (monsters.Count == 0 || _elapsedWaveTime >= TimeUntilWaveTwo)){
+            if(WaveTwoLaunched == false){
+                WaveTwoLaunched = true;
+                _elapsedWaveTime = Mathf.NegativeInfinity;
+                foreach (var _spawn in Spawners_Wave2)
             {
-                door.SetTrigger("Opening");
+                monsters.Add(_spawn.SpawnAnEnemy());
             }
+            }
+            else{
+                Debug.Log("wave ended");
+                _musicManager.ChangeSituation(2);
+                foreach (var door in DoorsAnimator)
+                {
+                    door.SetTrigger("Opening");
+                }
 
-            this.enabled = false;
+                activeRoom = false;
+                this.enabled = false;
+            }
         }
     }
 }
